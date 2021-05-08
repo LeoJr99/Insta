45c63b7e68c6b71d264b3389c3777d33d95e326b
Leo <lvaldiiva.jr@gmail.com>
2021-05-07 19:51:16 -0700

    Insta Project Push

--- Insta/AppDelegate.swift
+++ Insta/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "3ssOMBjttL2IvRS826fM6m1m6D4KUaS3F83aN4Yz"
+                    $0.clientKey = "QdIhhEv8hgyiOSp1VSaOK1epdv0fhpKkoTV2MuqE" 
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Insta/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Insta
+//
+//  Created by Leonardo Valdivia on 5/5/21.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
